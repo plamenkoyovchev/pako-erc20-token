7fd748a0b7fd7ac0d3760f6bb84d3eaa0e53ab83
plamenkoyovchev <plamenkooo@gmail.com>
2023-01-22 19:08:24 +0200

    Use toWei util.

--- scripts/airdrop.js
+++ scripts/airdrop.js
@@ -1,11 +1,9 @@
 // https://dev.to/yakult/a-concise-hardhat-tutorial-part-2-writing-erc20-2jpm
 
 const { ethers } = require("hardhat");
+const { toWei } = require("../utils/utils");
 
 const address = "0xd90F74f68BFb40FEa89b37019F80A04ea6D4524e";
-
-const fromWei = ethers.utils.formatEther;
-const toWei = ethers.utils.parseEther;
 
 async function main() {
 	const pakoToken = await ethers.getContractAt("PakoToken", address);
